4d983da84db48a0c8399bfb322887fb804a309eb
pratyush <pratyushchauhan62@gmail.com>
2025-03-18 23:47:09 +0530

    misc

--- .config/nvim/lua/cipher/plugins/telescope.lua
+++ .config/nvim/lua/cipher/plugins/telescope.lua
@@ -4,17 +4,15 @@ return {
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
     },
     config = function()
-        config = function()
-        require('telescope').setup {
-            pickers = {
-                find_files = {
-                    theme = "ivy"
-                } },
-            extensions = {
-                fzf = {}
-            }
-        }
-        end
+        --require('telescope').setup {
+        --    pickers = {
+        --        find_files = {
+        --            theme = "ivy"
+        --        } },
+        --    extensions = {
+        --        fzf = {}
+        --    }
+        --}
         require('telescope').load_extension('fzf')
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
